6c211ac0d420a637c4f796f8a64a9ac3a3f2fbf5
Jannis Erhard <jannis.erhard@gmx.de>
2021-06-26 16:33:25 +0200

    Added function which reads header into dictionary

--- andre_2.0.py
+++ andre_2.0.py
@@ -11,7 +11,7 @@ x=[]
 y=[]
 
 def read_and_parse_current_file(filename):
-    '''Open, Read and Parse file filename'''
+    '''Open, Read and Parse file filename. Output left and right column of file.'''
     
     file=open(filename,"r")
     lines=file.readlines()
@@ -22,29 +22,43 @@ def read_and_parse_current_file(filename):
             right.append("Empty_Line")
             left.append("Empty_Line")
         else:
-    # in case of normal line 
-#           left.append(((line.strip().split(","))[0]).strip())
-#           right.append(((line.strip().split(","))[1].strip()))
-           left.append(((line.split(","))[0]).strip())
-           right.append(((line.split(","))[1].strip()))
+    # in case of normal line, split at "," get rid of blanks 
+           left.append(((line.split(","))[0]).strip().replace(":",""))
+           right.append(((line.split(","))[1].strip()).replace(":",""))
     file.close()
     return left, right
 
+def get_ordinate_and_coordinate(left,right):
+    '''Take left and right column of quatsch file and extract data. Output xlabel,ylabel,ordinate,coordinate'''
+    startpoint=left.index("Voltage")
+    endpoint=left.index("Empty_Line")
+    
+    xlabel=left[startpoint]
+    ylabel=right[startpoint]
+    for i in range(startpoint+1,endpoint):
+        x.append(float(left[i]))
+        y.append(float(right[i]))
+    return xlabel, ylabel, x, y
+
+def read_header_into_dict(left,right):
+    '''Reads the header of ``quatsch'' - Files into dictionary'''
+    endpoint=left.index("Voltage")
+    header = {}
+    for i in range(0,endpoint):
+        header[str(left[i])]=right[i]
+    return header 
+
 left, right = read_and_parse_current_file(filename)
+xlabel, ylabel, x, y = get_ordinate_and_coordinate(left,right)
+header = read_header_into_dict(left,right)
 
-startpoint=left.index("Voltage:")
-endpoint=left.index("Empty_Line")
-
-xlabel=left[startpoint]
-ylabel=right[startpoint]
-for i in range(startpoint+1,endpoint):
-    x.append(float(left[i]))
-    y.append(float(right[i]))
 
 
 plt.plot(x,y)
 plt.xlabel(xlabel)
 plt.ylabel(ylabel)
+plt.title(header['Title'])
+#print(header.keys())
+#print(header.values())
 plt.show()
-print(len(x),len(y))
 
